7a132aad1db1a9b41fc83bf255d453f14ba0b02f
Bogdan <bogdan.oloeriu@gmail.com>
2023-11-07 22:11:11 +0100

    added payment endpoint

--- vxbank-api/src/main/java/eu/vxbank/api/controlers/PaymentsEndpoint.java
+++ vxbank-api/src/main/java/eu/vxbank/api/controlers/PaymentsEndpoint.java
@@ -1,5 +1,9 @@
 package eu.vxbank.api.controlers;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentsEndpoint {
 
     @PostMapping("/payments/create-checkout-session")
-    public String createCheckoutSession(){
-        return "Hello checkout session";
+    public ResponseEntity<String> createCheckoutSession() throws StripeException {
+        String YOUR_DOMAIN = "http://localhost:4242";
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setSuccessUrl(YOUR_DOMAIN + "/success.html")
+                        .setCancelUrl(YOUR_DOMAIN + "/cancel.html")
+                        .setAutomaticTax(
+                                SessionCreateParams.AutomaticTax.builder()
+                                        .setEnabled(true)
+                                        .build())
+                        .addLineItem(
+                                SessionCreateParams.LineItem.builder()
+                                        .setQuantity(1L)
+                                        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                                        .setPrice("{{PRICE_ID}}")
+                                        .build())
+                        .build();
+        Session session = Session.create(params);
+        String response = session.getUrl();
+        response = "Hello checkout response: " + response;
+        return ResponseEntity.ok(response);
     }
 }
