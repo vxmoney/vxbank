21e9139f6d38843e49b1e987d1c787a59b4b81f1
bogdan <bogdan.oloeriu@gmail.com>
2023-11-11 16:23:44 +0100

    decoded query params without router

--- vxbank-web-app/src/app/vxpayment/sucess/page.js
+++ vxbank-web-app/src/app/vxpayment/sucess/page.js
@@ -1,6 +1,9 @@
-export default function SuccessPage({searchParams}) {
-
-  console.log(searchParams)
+export default function SuccessPage({ searchParams }) {
+  let { stripeSessionId, projectId, clubId } = searchParams;
+  
+  console.log(stripeSessionId);
+  console.log(projectId);
+  console.log(clubId);
   //console.log(router.query);
 
   return <p>Hello vxpayment/success/page page</p>;
