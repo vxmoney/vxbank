e33391679d957dd4ef913e14843e2e03abc2e752
bogdan <bogdan.oloeriu@gmail.com>
2023-11-12 10:15:33 +0100

    added create and get test for

--- vxbank-datastore/vxdatastore/src/test/java/vxbank/datastore/TestVxServiceIntegration.java
+++ vxbank-datastore/vxdatastore/src/test/java/vxbank/datastore/TestVxServiceIntegration.java
@@ -1,14 +1,25 @@
 package vxbank.datastore;
 
+import com.googlecode.objectify.Key;
 import org.junit.jupiter.api.Test;
+import vxbank.datastore.data.models.VxDataTestEntity;
+import vxbank.datastore.data.models.VxServiceIntegration;
 
 import java.util.Optional;
 
 
 public class TestVxServiceIntegration {
 
+
     @Test
-    void testCreateGet(){
+    void testCreateGet() {
         VxBankDatastore ds = VxBankDatastore.init("my-project", VxBankDatastore.ConnectionType.localhost, Optional.empty());
+
+        VxServiceIntegration vxServiceIntegration = new VxServiceIntegration();
+        vxServiceIntegration.id = 1L;
+        vxServiceIntegration.title = "chessout-integration-test";
+        vxServiceIntegration.description = "Integration test description";
+        Key<VxServiceIntegration> key = ds.ofy.save().entity(vxServiceIntegration).now();
+
     }
 }
