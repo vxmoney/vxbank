2d6f445c109441053f879d4b1548cf404975fa25
Bogdan <bogdan.oloeriu@gmail.com>
2023-11-15 23:29:34 +0100

    initialized VxStripeKeys

--- vxbank-api/src/main/java/eu/vxbank/api/utils/components/VxStripeKeys.java
+++ vxbank-api/src/main/java/eu/vxbank/api/utils/components/VxStripeKeys.java
@@ -1,15 +1,30 @@
 package eu.vxbank.api.utils.components;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VxStripeKeys {
+    @Autowired
+    private SystemService systemService;
+    public String secretKey;
+    public String webhookSigningSecret;
 
 
+    @PostConstruct
+    public void init() {
+        switch (systemService.environment) {
+            case LOCALHOST:
+            case DEVELOPMENT:
+                this.secretKey =
+                        "sk_test_51O93vKB6aHGAQTGCjNsNa75J2T8ilFFZpS4a441LBEceglDwUnll3GvpzaeIvCkw6nnWgFxsQY2J34ex4oJjoinm00TmBT4a0b";
+                this.webhookSigningSecret = "whsec_J71Mv8Nl89K2iCgaXjmOXazVlktirOPv";
+                return;
+            default:
+                throw new IllegalStateException("Not supported environment: " + systemService.environment);
 
-    public void init(@Autowired SystemService systemService) {
-
+        }
     }
 
 }
