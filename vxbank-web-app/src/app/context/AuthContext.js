999fae32cc29a2ec9b72a0b223dd6df374587264
Bogdan <bogdan.oloeriu@gmail.com>
2023-11-18 14:51:58 +0100

    extended auth context

--- vxbank-web-app/src/app/context/AuthContext.js
+++ vxbank-web-app/src/app/context/AuthContext.js
@@ -1,12 +1,39 @@
 "use client";
 import { useContext, createContext, useState, useEffect } from "react";
+import {
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { auth } from "../firebase";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState("Bogdan")
-  
-  return <AuthContext.Provider value={{user}}>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState(null);
+
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  const logout = () => {
+    signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, [user]);
+
+  return (
+    <AuthContext.Provider value={{ user, googleSignIn, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
