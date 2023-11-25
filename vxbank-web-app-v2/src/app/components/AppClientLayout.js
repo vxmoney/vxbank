f225f9dc050c6578fbdc5deb84520881339fa995
Bogdan <bogdan.oloeriu@gmail.com>
2023-11-25 15:19:22 +0100

    wired the default app

--- vxbank-web-app-v2/src/app/components/AppClientLayout.js
+++ vxbank-web-app-v2/src/app/components/AppClientLayout.js
@@ -1,9 +1,12 @@
+"use client";
+import { AuthContextProvider } from "../context/AuthContext";
 import Navbar from "./Navbar";
 
 export default function AppClientLayout({ children }) {
   return (
-    <>
-      <Navbar /> {children}
-    </>
+    <AuthContextProvider>
+      <Navbar />
+      {children}
+    </AuthContextProvider>
   );
 }
