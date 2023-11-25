4a79a61daa79104227266b3a480306f78b95477a
Bogdan <bogdan.oloeriu@gmail.com>
2023-11-25 14:50:43 +0100

    added navbar

---
+++ vxbank-web-app-v2/src/app/about/page.js
@@ -0,0 +1,5 @@
+export default function About(){
+    return(
+        <div className="p-4">About page</div>
+    )
+}
---
+++ vxbank-web-app-v2/src/app/profile/page.js
@@ -0,0 +1,5 @@
+export default function Profiler(){
+    return (
+        <div className="p-4">Profile page</div>
+    )
+}
