4e824a93273216d01fcf774a4a13afca345cb2b1
Bogdan <bogdan.oloeriu@gmail.com>
2023-11-25 14:57:29 +0100

    login

--- vxbank-web-app-v2/src/app/components/Navbar.js
+++ vxbank-web-app-v2/src/app/components/Navbar.js
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-20 w-full border-b-2 flex items-center justify-between p-2">
+      <ul className="flex">
+        <li className="p-2 cursor-pointer">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="p-2 cursor-pointer">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="p-2 cursor-pointer">
+          <Link href="/profile">Profile</Link>
+        </li>
+      </ul>
+      <ul className="flex">
+        <li className="p-2 cursor-pointer">Login</li>
+        <li className="p-2 cursor-pointer">Logout</li>
+      </ul>
+    </div>
+  );
 }
