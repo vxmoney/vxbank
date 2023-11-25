51978648c8a2b419eca71dcd0cc651f9b6d4a7f1
Bogdan <bogdan.oloeriu@gmail.com>
2023-11-25 16:05:05 +0100

    implemented sign in and sign out

--- vxbank-web-app-v2/src/app/components/Navbar.js
+++ vxbank-web-app-v2/src/app/components/Navbar.js
@@ -2,9 +2,25 @@ import Link from "next/link";
 import { UserAuth } from "../context/AuthContext";
 
 export default function Navbar() {
-  const {user} = UserAuth();
+  const { user, googleSignIn, logOut } = UserAuth();
 
   console.log("Navbar user", user);
+  const handleSignIn = async () => {
+    console.log("Handle sign in");
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="h-20 w-full border-b-2 flex items-center justify-between p-2">
@@ -20,8 +36,10 @@ export default function Navbar() {
         </li>
       </ul>
       <ul className="flex">
-        <li className="p-2 cursor-pointer">Login</li>
-        <li className="p-2 cursor-pointer">Logout</li>
+        <li onClick={handleSignIn} className="p-2 cursor-pointer">
+          Login
+        </li>
+        <li onClick={handleSignOut} className="p-2 cursor-pointer">Logout</li>
       </ul>
     </div>
   );
