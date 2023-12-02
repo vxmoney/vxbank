478067df91a51d44d7b55b528e44c7722517444f
Bogdan <bogdan.oloeriu@gmail.com>
2023-12-02 16:58:11 +0100

    created user on localhost emulator

--- vxbank-api/src/test/java/eu/vxbank/api/user/TestUserFlow.java
+++ vxbank-api/src/test/java/eu/vxbank/api/user/TestUserFlow.java
@@ -2,19 +2,43 @@ package eu.vxbank.api.user;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
 import org.junit.jupiter.api.Test;
 
 public class TestUserFlow {
 
+    private static UserRecord createUserWithEmail(FirebaseAuth firebaseAuth, String email) throws
+            FirebaseAuthException {
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setEmail(email)
+                .setEmailVerified(false)
+                .setPassword("secure-password"); // Set a secure password for the user
+
+        return firebaseAuth.createUser(request);
+    }
+
     @Test
-    void deprecatedPaymentTest() throws Exception {
+    void testOauth() throws Exception {
 
-        String projectId = "my-project-id";
+        String projectId = "vxbank-eu-dev";
+        FirebaseApp.initializeApp();
 
-        // Initialize the Firebase Admin SDK
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(FirebaseApp.getInstance(projectId));
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        String email = "user@example.com";
+
+        UserRecord userRecord = createUserWithEmail(firebaseAuth, email);
+        String customToken = generateCustomToken(firebaseAuth, userRecord.getUid());
+
+
+        System.out.println("Custom Token: " + customToken);
+
 
 
         System.out.println("End of test");
+    }
+
+    private static String generateCustomToken(FirebaseAuth firebaseAuth, String uid) throws FirebaseAuthException {
+        return firebaseAuth.createCustomToken(uid);
     }
 }
