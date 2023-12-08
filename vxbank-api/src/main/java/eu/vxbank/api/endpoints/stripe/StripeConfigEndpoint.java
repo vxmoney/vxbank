7e27a6594fc2854e8ff8ca7268d63e239584f398
Bogdan <bogdan.oloeriu@gmail.com>
2023-12-08 16:44:15 +0100

    implemented getByUserId

--- vxbank-api/src/main/java/eu/vxbank/api/endpoints/stripe/StripeConfigEndpoint.java
+++ vxbank-api/src/main/java/eu/vxbank/api/endpoints/stripe/StripeConfigEndpoint.java
@@ -4,10 +4,13 @@ import eu.vxbank.api.endpoints.stripe.dto.StripeConfigGetByUserIdResponse;
 import eu.vxbank.api.services.VxFirebaseAuthService;
 import eu.vxbank.api.utils.components.SystemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vxbank.datastore.VxBankDatastore;
+import vxbank.datastore.data.models.VxStripeConfig;
+import vxbank.datastore.data.service.VxService;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/stripeConfig")
@@ -20,12 +23,21 @@ public class StripeConfigEndpoint {
     SystemService systemService;
 
     @GetMapping("/getByUserId/{userId}")
+    @ResponseBody
     public StripeConfigGetByUserIdResponse getByUserId(@PathVariable("userId") Long userId) {
 
-        StripeConfigGetByUserIdResponse response = new StripeConfigGetByUserIdResponse();
-        response.state = null;
+        VxBankDatastore ds = systemService.getVxBankDatastore();
+        List<VxStripeConfig> stripeConfigs = VxService.getByUserId(userId, new HashMap<>(), ds, VxStripeConfig.class);
 
-        throw new IllegalStateException("Please implement this");
+        StripeConfigGetByUserIdResponse response = new StripeConfigGetByUserIdResponse();
+        if (stripeConfigs.size() > 0) {
+            VxStripeConfig config = stripeConfigs.get(0);
+            response.state = config.state;
+        } else {
+            response.state = VxStripeConfig.State.notConfigured;
+        }
+
+        return response;
     }
 
 }
