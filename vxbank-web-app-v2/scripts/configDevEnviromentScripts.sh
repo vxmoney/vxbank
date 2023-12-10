f00ce5550a126912591d93a170e1f0649e406fec
Bogdan <bogdan.oloeriu@gmail.com>
2023-12-10 12:52:03 +0100

    renamed the config dev scripts

--- vxbank-web-app-v2/scripts/configDevEnviromentScripts.sh
+++ vxbank-web-app-v2/scripts/configDevEnviromentScripts.sh
@@ -1,16 +1,10 @@
-set-env-stg() {
-    cp config/stg-firebase.js src/app/firebase.js
-    echo "Curent env = staging"
-}
-
-set-env-dev() {
+initEnvDevelopment() {
     cp config/dev-firebase.js src/app/firebase.js
     echo "Curent env = development"
 }
 
-deploy-on-dev(){
-    set-env-dev
-    rm -rf build
+deployOnDev(){
+    initEnvDevelopment
     gcloud config set project vxbank-eu-dev
     gcloud config get-value project
     npm run build
