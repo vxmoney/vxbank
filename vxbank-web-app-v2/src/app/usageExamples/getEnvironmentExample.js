476a0b3f4d96b9cf4e03e68b89dbe9be9e29e9cd
Bogdan <bogdan.oloeriu@gmail.com>
2023-12-10 19:42:28 +0100

    formatted button

--- vxbank-web-app-v2/src/app/usageExamples/getEnvironmentExample.js
+++ vxbank-web-app-v2/src/app/usageExamples/getEnvironmentExample.js
@@ -3,21 +3,30 @@ import { useState } from "react";
 
 export default function GetEnvironmentExample() {
   const [pingResponse, setPingResponse] = useState(null);
+  const [formattedResponse, setFormattedResponse] = useState(null);
 
   const fetchPing = async () => {
     try {
       const response = await pingAPI.getEnvironment();
+      const formattedResponse = JSON.stringify(response.data, null, 2);
       setPingResponse(response.data);
+      setFormattedResponse(formattedResponse);
     } catch (error) {
       console.log("fetchPingError: ", error);
     }
   };
 
   return (
-    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+    <div>
       <div>Get environment example hello</div>
-      <button onClick={fetchPing}>Fetch ping</button>
-      {pingResponse && <div>{pingResponse.environment}</div>}
+      <button
+        onClick={fetchPing}
+        type="button"
+        class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        Fetch ping
+      </button>
+      {formattedResponse && <pre>{formattedResponse}</pre>}
     </div>
   );
 }
--- vxbank-web-app-v2/src/app/usageExamples/page.js
+++ vxbank-web-app-v2/src/app/usageExamples/page.js
@@ -7,36 +7,39 @@ export default function UsageExamples() {
   const [activeTab, setActiveTab] = useState("Ping");
 
   return (
-    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-      <ul class="flex flex-wrap -mb-px">
-        <li class="me-2">
-          <a
-            href="#"
-            class={`inline-block p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${
-              activeTab === "Ping"
-                ? "border-b-2 text-blue-600 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
-                : ""
-            }`}
-            onClick={() => setActiveTab("Ping")}
-          >
-            Ping
-          </a>
-        </li>
-        <li class="me-2">
-          <a
-            href="#"
-            class={`inline-block p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${
-              activeTab === "Hello"
-                ? "border-b-2 text-blue-600 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
-                : ""
-            }`}
-            onClick={() => setActiveTab("Hello")}
-          >
-            Hello
-          </a>
-        </li>
-      </ul>
-      {activeTab === "Ping" && <GetEnvironmentExample/>}
+    <div>
+      <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        <ul class="flex flex-wrap -mb-px">
+          <li class="me-2">
+            <a
+              href="#"
+              class={`inline-block p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${
+                activeTab === "Ping"
+                  ? "border-b-2 text-blue-600 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
+                  : ""
+              }`}
+              onClick={() => setActiveTab("Ping")}
+            >
+              Ping
+            </a>
+          </li>
+          <li class="me-2">
+            <a
+              href="#"
+              class={`inline-block p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${
+                activeTab === "Hello"
+                  ? "border-b-2 text-blue-600 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
+                  : ""
+              }`}
+              onClick={() => setActiveTab("Hello")}
+            >
+              Hello
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {activeTab === "Ping" && <GetEnvironmentExample />}
       {activeTab === "Hello" && <div>Hello content</div>}
     </div>
   );
