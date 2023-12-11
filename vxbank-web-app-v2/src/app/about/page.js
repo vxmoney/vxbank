4f3b97cc4376e4b4f49dcdd46d71754b049a474b
Bogdan <bogdan.oloeriu@gmail.com>
2023-12-11 03:22:27 +0100

    added about

--- vxbank-web-app-v2/src/app/about/page.js
+++ vxbank-web-app-v2/src/app/about/page.js
@@ -1,5 +1,32 @@
-export default function About(){
-    return(
-        <div className="p-4">About page</div>
-    )
+export default function About() {
+  return (
+    <div className="p-4">
+      <div class="block max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          About: VxGaming
+        </h5>
+
+        <p class="p-4 font-normal text-gray-700 dark:text-gray-400">
+          🌟 Get ready to turn your gaming passion into PROFIT with VxGaming!
+          🎮💰
+        </p>
+
+        <p class="p-4 font-normal text-gray-700 dark:text-gray-400">
+          🚀 Join real money tournaments and transform your skills into a
+          lucrative business! 🌐💼
+        </p>
+        <p class="p-4 font-normal text-gray-700 dark:text-gray-400"></p>
+        <p class="p-4 font-normal text-gray-700 dark:text-gray-400">
+          🏆 Compete, Win, EARN! 🏆
+        </p>
+        <p class="p-4 font-normal text-gray-700 dark:text-gray-400">
+          Ready to level up your gaming journey? Sign up now at VxGaming.com!
+          🚀🎮💸
+        </p>
+        <p class="p-4 font-normal text-gray-700 dark:text-gray-400">
+          #VxGaming #GameToEarn #VxGamingRevolution
+        </p>
+      </div>
+    </div>
+  );
 }
