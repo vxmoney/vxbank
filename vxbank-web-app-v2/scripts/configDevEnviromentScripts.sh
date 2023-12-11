d1a1af2ee70c4ae1a08e56c5010fa2026beeef57
Bogdan <bogdan.oloeriu@gmail.com>
2023-12-11 02:06:25 +0100

    updated scripts

--- vxbank-web-app-v2/scripts/configDevEnviromentScripts.sh
+++ vxbank-web-app-v2/scripts/configDevEnviromentScripts.sh
@@ -1,13 +1,15 @@
 initEnvDevelopment() {
     cp scripts/dev-firebase.js src/app/firebase.js
+    cp scripts/dev-apiConfig.js src/api/apiConfig.js
     echo "Curent env = development"
 }
-initEnvLocalhostDevelopment(){
+initEnvLocalhost() {
     cp scripts/dev-firebase.js src/app/firebase.js
     cp scripts/localhost-apiConfig.js src/api/apiConfig.js
+    echo "Curent env = localhost"
 }
 
-deployOnDev(){
+deployOnDev() {
     initEnvDevelopment
     gcloud config set project vxbank-eu-dev
     gcloud config get-value project
