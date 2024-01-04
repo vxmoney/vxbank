8dec4b4d94c542f8d5fb238adcd5784a4c5166f7
Bogdan <bogdan.oloeriu@gmail.com>
2024-01-04 16:12:15 +0100

    modal works

--- vxbank-web-app-v2/src/app/events/page.js
+++ vxbank-web-app-v2/src/app/events/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 import CreateEventModal from "../components/CreateEventModal";
 
@@ -10,7 +10,12 @@ export default function EventsHome() {
           <p class="mb-2 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-8 dark:text-gray-400">
             -- Events --
           </p>
-          <button class="flex items-center">
+          <button
+            data-modal-target="default-modal"
+            data-modal-toggle="default-modal"
+            class="flex items-center"
+            type="button"
+          >
             <svg
               class="w-6 h-6 text-gray-500 dark:text-gray-400 mr-2"
               aria-hidden="true"
@@ -25,14 +30,8 @@ export default function EventsHome() {
         </div>
       </section>
 
-      <button
-        data-modal-target="default-modal"
-        data-modal-toggle="default-modal"
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-        Toggle modal
-      </button>
+      
+      
       <CreateEventModal />
     </div>
   );
