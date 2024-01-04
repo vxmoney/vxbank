ffd231b5964eafa569a4e2983940aef86610bf2c
Bogdan <bogdan.oloeriu@gmail.com>
2024-01-04 16:22:32 +0100

    justified centered

--- vxbank-web-app-v2/src/app/components/CreateEventModal.js
+++ vxbank-web-app-v2/src/app/components/CreateEventModal.js
@@ -43,7 +43,7 @@ const CreateEventModal = () => {
       id="default-modal"
       tabIndex="-1"
       aria-hidden="true"
-      className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="hidden overflow-y-auto overflow-x-hidden fixed inset-0 z-50 flex items-center justify-center"
     >
       <div className="relative p-4 w-full max-w-2xl max-h-full">
         {/* Modal content */}
