435740a8f1976b9750290b82436dcb8e6b6fb32d
Bogdan <bogdan.oloeriu@gmail.com>
2024-01-04 15:32:34 +0100

    added create event button

--- vxbank-web-app-v2/src/app/events/page.js
+++ vxbank-web-app-v2/src/app/events/page.js
@@ -1,7 +1,25 @@
 export default function EventsHome() {
   return (
     <div>
-      <h1>Events Home</h1>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="py-2 px-4 mx-auto max-w-screen-xl lg:py-2 flex justify-between items-center">
+          <p class="mb-2 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-8 dark:text-gray-400">
+            -- Events --
+          </p>
+          <button class="flex items-center">
+            <svg
+              class="w-6 h-6 text-gray-500 dark:text-gray-400 mr-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 18 20"
+            >
+              <path d="M16 8H10V2a2 2 0 0 0-4 0v6H2a2 2 0 0 0 0 4h4v6a2 2 0 0 0 4 0v-6h6a2 2 0 0 0 0-4Z" />
+            </svg>
+            <span class="text-gray-500 dark:text-gray-400">Create event</span>
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
