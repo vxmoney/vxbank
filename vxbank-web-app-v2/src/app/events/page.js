5f19ec61c5bd54c69c5a0a1d3217986facdb6c4d
Bogdan <bogdan.oloeriu@gmail.com>
2024-01-04 16:01:19 +0100

    I can crate modal

--- vxbank-web-app-v2/src/app/events/page.js
+++ vxbank-web-app-v2/src/app/events/page.js
@@ -1,3 +1,7 @@
+"use client"
+import { useState } from "react";
+import CreateEventModal from "../components/CreateEventModal";
+
 export default function EventsHome() {
   return (
     <div>
@@ -20,6 +24,16 @@ export default function EventsHome() {
           </button>
         </div>
       </section>
+
+      <button
+        data-modal-target="default-modal"
+        data-modal-toggle="default-modal"
+        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+      >
+        Toggle modal
+      </button>
+      <CreateEventModal />
     </div>
   );
 }
