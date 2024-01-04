3f268c94f29db8d6a15db103e369bc633f35ecb6
Bogdan <bogdan.oloeriu@gmail.com>
2024-01-04 16:42:22 +0100

    accept does not nothing

--- vxbank-web-app-v2/src/app/components/CreateEventModal.js
+++ vxbank-web-app-v2/src/app/components/CreateEventModal.js
@@ -1,5 +1,12 @@
 import { useEffect } from "react";
 const CreateEventModal = () => {
+
+  const closeModal = () => {
+    const modal = document.getElementById("default-modal");
+    modal.classList.add("hidden");
+    modal.setAttribute("aria-hidden", "true");
+  };
+
   useEffect(() => {
     const modalButton = document.querySelector(
       '[data-modal-toggle="default-modal"]'
@@ -94,7 +101,7 @@ const CreateEventModal = () => {
           {/* Modal footer */}
           <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
             <button
-              data-modal-hide="default-modal"
+              
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
@@ -104,6 +111,7 @@ const CreateEventModal = () => {
               data-modal-hide="default-modal"
               type="button"
               className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              
             >
               Decline
             </button>
