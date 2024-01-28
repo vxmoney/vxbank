a49d8749997576eaba41bc73ea255ca0622c570b
Bogdan <bogdan.oloeriu@gmail.com>
2024-01-28 12:33:23 +0100

    added instruction about tests that need special configuration

--- vxbank-api/src/test/java/eu/vxbank/api/stripe/SideLinkUserTest.java
+++ vxbank-api/src/test/java/eu/vxbank/api/stripe/SideLinkUserTest.java
@@ -34,6 +34,19 @@ public class SideLinkUserTest {
     @Autowired
     SystemService systemService;
 
+    /**
+     * How to run this test.
+     * Start local server
+     * - bootRun
+     * Configure frontend and connect to local host
+     * - openVsCode
+     * - . scripts/configDevEnviromentScripts.sh
+     * - initEnvLocalhost
+     * - npn install
+     * - npn run dev
+     * - /DeveloperExamples/Localhost auth/ Generate user and login
+     * Use the vxtoken generated and paste in this text.
+     */
     @Test
     public void sideLinkUserAndAddFundsTest() throws FirebaseAuthException, JsonProcessingException {
 
@@ -43,7 +56,7 @@ public class SideLinkUserTest {
             return;
         }
 
-        String vxToken = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI4IiwidWlkIjo4LCJpc3MiOiJzZWxmIiwiZXhwIjoxNzA0NTU4OTk2LCJpYXQiOjE3MDQ1NTE3OTYsImVtYWlsIjoidXNlcl8xNzA0NTUxNzk1NjQ4QGV4YW1wbGUuY29tIn0.WkPQ1gHFsoOdfCVp7Ldr1tk4hUopdaryRoiQ5U6hIGa6FiXAXr0RwqJy0mKEhgJjmuu7m1LqJbCE83s5H6JuTTN1uE98bwOZMD9y1iFXrc0rD2TF36jQt3kptWZusxDZYeecSzuzEyiT1gHfkCgsX-utJSTrtnWBavMcHwptOo5HsZ--HcZ5sSmukgow_3eEzZcYhDC9e49odIGRYGR6KaiYSGnt9CBsKIyvPcWn6G54KsVkFlrywnU-oeUG2cVR_2_2ikMp9TMwx1FbA_WK_un12vFo0PzorHn6vpowTY7s9vRTqO8J2KDzDrffXIAqM8likbnNRTOTv_4cJ7n6lA";
+        String vxToken = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxMjQiLCJ1aWQiOjEyNCwiaXNzIjoic2VsZiIsImV4cCI6MTcwNjQ0ODQ1MCwiaWF0IjoxNzA2NDQxMjUwLCJlbWFpbCI6InVzZXJfMTcwNjQ0MTI1MDY1NEBleGFtcGxlLmNvbSJ9.Fb7pRkIh6pYiJfhVINYTdpb0Nu4eA_AzIFaSmVf54-ymQMTRl6dxU0bzn9ghfwMrZ3uyBbIcwEXg96QPMN0Vg2CcY_ENVu7IJKNvNcCpRx6imEV9XGMvBl2KsubPzD8XFMzYp-69TCStyJ8i97baXmE9lwMyYBKu_Cm42Sn1fGazowqtqBq_mN0khmW4oIS_ohXklICBg6NA5KyzXboEHbjgBJubzNKRjW5r1VzH1Yq4PcyVI1sZHQJSc9nocQcTN5WR-YHn_Z1aQoPiibbu8mJI3TY7iedTeCo0SVrBrFm770a1crGpSLxcrghxCPwSgaFmpHs6He4kRaPp2IVXgA";
         String stripeId = "acct_1OUyNpB1kQZowFak";
 
         LoginResponse loginResponse = PingHelper.whoAmI(vxToken, restTemplate, port, 200);
