c8ca3ed103d6efb312bcd4a64a403f017432ef23
Bogdan <bogdan.oloeriu@gmail.com>
2024-02-10 09:51:31 +0100

    Client can not process respective currency

--- vxbank-api/src/main/java/eu/vxbank/api/endpoints/ping/PingEndpoint.java
+++ vxbank-api/src/main/java/eu/vxbank/api/endpoints/ping/PingEndpoint.java
@@ -201,12 +201,13 @@ public class PingEndpoint {
                         VxStripeConfig.class)
                 .get(0);
 
-        List<Funds> clientFundsList =
-                VxStripeUtil.getFundsList(stripeKeys.stripeSecretKey, vxStripeConfig.stripeAccountId);
-        Optional<Funds> currentClientFundsForSpecificCurrency = clientFundsList.stream().filter(funds -> funds.currency.equals(
-                params.currency        )).findFirst();
-        if (currentClientFundsForSpecificCurrency.isEmpty()){
-            throw new IllegalStateException("Client can not process respective currency. "+ params.currency);
+        List<Funds> clientFundsList = VxStripeUtil.getFundsList(stripeKeys.stripeSecretKey,
+                vxStripeConfig.stripeAccountId);
+        Optional<Funds> currentClientFundsForSpecificCurrency = clientFundsList.stream()
+                .filter(funds -> funds.currency.equals(params.currency))
+                .findFirst();
+        if (currentClientFundsForSpecificCurrency.isEmpty()) {
+            throw new IllegalStateException("Client can not process respective currency. " + params.currency);
         }
 
         VxStripeUtil.sendFundsToStripeAccount(stripeKeys.stripeSecretKey,
