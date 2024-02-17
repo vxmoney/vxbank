0dbb5b81b4cc6c45f4dd80fa0f6da62435b527e6
Bogdan <bogdan.oloeriu@gmail.com>
2024-02-17 11:31:56 +0100

    added ativate different currecies tab

---
+++ vxbank-web-app-v2/src/app/components/stripe/StripeActivateDifferentCurrenciesComponent.js
@@ -0,0 +1,125 @@
+import { useEffect, useState } from "react";
+import configValues from "../../../api/apiConfig";
+import { stripeConfigAPI } from "@/api/stripeConfig";
+import { userAPI } from "@/api/user";
+import { UserAuth } from "@/app/context/AuthContext";
+import { useSearchParams } from "next/navigation";
+
+/**
+ * How it works
+ * Check stripe config endpoint: stripeConfig/getByUserId/{userId}
+ */
+
+const StripeActivateDifferentCurrenciesComponent = ({ id, email, name }) => {
+  const searchParams = useSearchParams();
+  const configStatus = searchParams.get("configStatus");
+  const { frontendPort, frontendBaseUrl, frontendProtocol } = configValues;
+  const { vxUserInfo, setVxUserInfo } = UserAuth();
+
+  const [stripeConfigState, setStripeConfigState] = useState(null);
+  const [configUrl, setConfigUrl] = useState(null);
+
+  const callGetStripeConfig = async () => {
+    try {
+      const getStripeConfigResponse = await stripeConfigAPI.getByUserId(
+        vxUserInfo.vxToken,
+        vxUserInfo.id
+      );
+      setStripeConfigState(getStripeConfigResponse.data.state);
+    } catch (error) {
+      console.error("callGetStripeConfig error: ", error);
+    }
+  };
+
+  const initiateResumeConfiguration = async () => {
+    let requestParams = {
+      userId: vxUserInfo.id,
+    };
+    try {
+      console.log("clicked initiate resume configuration");
+      //window.location.href = 'https://www.google.com'
+      const initiateResponse = await stripeConfigAPI.initiateConfig(
+        vxUserInfo.vxToken,
+        requestParams
+      );
+      console.log("initiateResponse", initiateResponse);
+      if (initiateResponse.data.configurationComplete) {
+        console.log("Time to refresh token");
+        const refreshResponse = await userAPI.refreshVxToken(
+          vxUserInfo.vxToken
+        );
+        console.log("refreshResponse", refreshResponse.data);
+        setVxUserInfo(refreshResponse.data);
+      } else {
+        let stripeUrl = initiateResponse.data.url;
+        window.location.href = stripeUrl;
+      }
+    } catch (error) {
+      console.error("initiateResumeConfiguration error", error);
+    }
+  };
+
+  useEffect(() => {
+    callGetStripeConfig();
+    console.log("ConfigStatus=", configStatus);
+    if (configStatus === "complete") {
+      // we got redirected by stripe so we initiate again.
+      // this will also handel the refresh part of the token
+      // backend will complete the back onboarding
+      // frontend will close activate component
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      initiateResumeConfiguration();
+    }
+  }, []);
+
+  return (
+    <div className="pl-8 pr-8 pt-8">
+      <div className="block  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Stripe activate different currencies
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400k">
+          Use this card to activate different currencies
+        </p>
+
+        <div className="flex-grow overflow-auto">
+          You will open your vxbank configuration page. You can activate
+          different currencies only using this tool. After you are done
+          validating your new currency account you wil have to refresh this page
+          in order to confirm that the currency is active
+        </div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Stripe config state
+              </th>
+              <td
+                className="px-6 py-4"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <p className="pr-2">{stripeConfigState} </p>
+
+                <button
+                  type="button"
+                  className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  onClick={initiateResumeConfiguration}
+                >
+                  Initiate / Resume configuration
+                </button>
+              </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Stripe callback value
+              </th>
+              <td className="px-6 py-4">{configStatus}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default StripeActivateDifferentCurrenciesComponent;
--- vxbank-web-app-v2/src/app/profile/page.js
+++ vxbank-web-app-v2/src/app/profile/page.js
@@ -3,6 +3,7 @@
 import { UserAuth } from "../context/AuthContext";
 import ProfileComponent from "../components/ProfileComponent";
 import StripeActivateComponent from "../components/stripe/StripeActivateComponent";
+import StripeActivateDifferentCurrenciesComponent from "../components/stripe/StripeActivateDifferentCurrenciesComponent";
 
 export default function Profiler() {
   const { vxUserInfo } = UserAuth();
@@ -16,6 +17,10 @@ export default function Profiler() {
       <ProfileComponent {...vxUserInfo} />
       {vxUserInfo.stripeConfigState !== "active" && (
         <StripeActivateComponent {...vxUserInfo} />
+      )}
+
+      {vxUserInfo.stripeConfigState === "active" && (
+        <StripeActivateDifferentCurrenciesComponent {...vxUserInfo} />
       )}
     </div>
   );
