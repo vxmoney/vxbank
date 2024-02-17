62c95fd648603985a1f95ef0b2fb92f30994ad0b
Bogdan <bogdan.oloeriu@gmail.com>
2024-02-17 12:10:16 +0100

    formatted top of activate different currencies

--- vxbank-web-app-v2/src/app/components/stripe/StripeActivateDifferentCurrenciesComponent.js
+++ vxbank-web-app-v2/src/app/components/stripe/StripeActivateDifferentCurrenciesComponent.js
@@ -35,7 +35,7 @@ const StripeActivateDifferentCurrenciesComponent = ({ id, email, name }) => {
   };
 
   return (
-    <div className="pl-8 pr-8 pt-8">
+    <div className="pl-8 pr-8">
       <div className="block  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Stripe activate different currencies
