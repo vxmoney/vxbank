fe8c32c9414e833719f38dcc741b2d63d3346000
Bogdan <bogdan.oloeriu@gmail.com>
2024-02-22 22:09:39 +0100

    implemented events card

--- vxbank-web-app-v2/src/app/components/LequeOfLegends1v1Component.js
+++ vxbank-web-app-v2/src/app/components/LequeOfLegends1v1Component.js
@@ -26,8 +26,59 @@ export default function LeagueOfLegends1v1Component() {
   }, [eventId]);
 
   return (
-    <di>
-      <h1>Hello nested id = : {eventId}</h1>
-    </di>
+    <div>
+      {eventData && (
+        <div className="pl-8 pr-8 pt-8">
+          <div className="block  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Event details
+            </h5>
+
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <tbody>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Event title
+                  </th>
+                  <td className="px-6 py-4">{eventData.title}</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Entry price
+                  </th>
+                  <td className="px-6 py-4">
+                    {eventData.entryPrice} {eventData.currency}
+                  </td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Game
+                  </th>
+                  <td className="px-6 py-4">{eventData.vxGame}</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Event type
+                  </th>
+                  <td className="px-6 py-4">{eventData.type}</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    State
+                  </th>
+                  <td className="px-6 py-4">{eventData.state}</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Available funds
+                  </th>
+                  <td className="px-6 py-4">{eventData.availableFunds}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
