30a91a5e0c8c5ded25186f75b8742660af514ede
Bogdan <bogdan.oloeriu@gmail.com>
2024-02-23 17:40:21 +0100

    added some alerts

--- vxbank-web-app-v2/src/app/components/events1v1/Join1v1EventModal.js
+++ vxbank-web-app-v2/src/app/components/events1v1/Join1v1EventModal.js
@@ -9,6 +9,16 @@ const Join1v1EventModal = ({ fetchParticipants }) => {
   const { vxUserInfo } = UserAuth();
   let { eventId } = useParams();
   const [eventData, setEventData] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("Nothing to show here");
+
+  function setMessageAndShowAlertForABit(message) {
+    setAlertMessage(message);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
+  }
 
   const eventJoinParams = {
     vxUserId: vxUserInfo?.id,
@@ -73,6 +83,7 @@ const Join1v1EventModal = ({ fetchParticipants }) => {
       })
       .catch((error) => {
         console.error("Error joining event:", error);
+        setMessageAndShowAlertForABit("Error joining event");
         // Handle error
       });
   };
@@ -94,9 +105,61 @@ const Join1v1EventModal = ({ fetchParticipants }) => {
       })
       .catch((error) => {
         console.error("Error close event:", error);
+        setMessageAndShowAlertForABit("Error closing event");
         // Handle error
       });
   };
+
+  
+
+  const myToast = showAlert && (
+    <div
+      id="toast-simple"
+      class="flex items-center w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800 fixed bottom-5 right-5"
+      role="alert"
+    >
+      <svg
+        class="w-5 h-5 text-blue-600 dark:text-blue-500 rotate-45"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 18 20"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"
+        />
+      </svg>
+      <div class="ps-4 text-sm font-normal">{alertMessage}</div>
+      <button
+        type="button"
+        class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        data-dismiss-target="#toast-warning"
+        aria-label="Close"
+        onClick={() => setShowAlert(false)}
+      >
+        <span class="sr-only">Close</span>
+        <svg
+          class="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 
   return (
     <div className="pt-8 pl-8">
@@ -224,6 +287,8 @@ const Join1v1EventModal = ({ fetchParticipants }) => {
           </div>
         </div>
       </div>
+
+      {myToast}
     </div>
   );
 };
