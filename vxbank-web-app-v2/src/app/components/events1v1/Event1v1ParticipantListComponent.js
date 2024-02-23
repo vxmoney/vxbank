8f7aba6ead239eeda1581e5e9f092e3bf53ffc52
Bogdan <bogdan.oloeriu@gmail.com>
2024-02-23 11:22:14 +0100

    formatted card

--- vxbank-web-app-v2/src/app/components/events1v1/Event1v1ParticipantListComponent.js
+++ vxbank-web-app-v2/src/app/components/events1v1/Event1v1ParticipantListComponent.js
@@ -37,34 +37,39 @@ export default function Event1v1ParticipantListComponent({ eventId }) {
   }, [participantsList]);
 
   return (
-    <div className="p-2 relative overflow-x-auto">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              gamerId
-            </th>
-            <th scope="col" className="px-6 py-3">
-              name
-            </th>
-          </tr>
-        </thead>
-        {participantsList && (
-          <tbody>
-            {participantsList.participantList.map((gamer) => (
-              <tr
-                key={gamer.id}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-              >
-                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  ---
-                </td>
-                <td className="px-6 py-4">{gamer.vxUserId}</td>
-              </tr>
-            ))}
-          </tbody>
-        )}
-      </table>
+    <div className="pl-8 pr-8 pt-8">
+      <div className="block  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Participant list
+        </h5>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                gamerId
+              </th>
+              <th scope="col" className="px-6 py-3">
+                name
+              </th>
+            </tr>
+          </thead>
+          {participantsList && (
+            <tbody>
+              {participantsList.participantList.map((gamer) => (
+                <tr
+                  key={gamer.id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    ---
+                  </td>
+                  <td className="px-6 py-4">{gamer.vxUserId}</td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
     </div>
   );
 }
