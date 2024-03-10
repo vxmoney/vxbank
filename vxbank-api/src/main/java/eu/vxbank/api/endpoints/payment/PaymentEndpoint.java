dc788e1abdd150bb1283a26ba8c6c18fbe67ac16
Bogdan <bogdan.oloeriu@gmail.com>
2024-03-10 19:24:30 +0100

    added createStripeSessionDepositFiatV2

--- vxbank-api/src/main/java/eu/vxbank/api/endpoints/payment/PaymentEndpoint.java
+++ vxbank-api/src/main/java/eu/vxbank/api/endpoints/payment/PaymentEndpoint.java
@@ -3,7 +3,11 @@ package eu.vxbank.api.endpoints.payment;
 
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Price;
 import com.stripe.model.checkout.Session;
+import com.stripe.param.InvoiceItemCreateParams;
+import com.stripe.param.PriceCreateParams;
+import com.stripe.param.checkout.SessionCreateParams;
 import eu.vxbank.api.endpoints.payment.dto.PaymentCreateParams;
 import eu.vxbank.api.endpoints.payment.dto.PaymentDepositFiatParams;
 import eu.vxbank.api.endpoints.payment.dto.PaymentDepositFiatResponse;
@@ -142,6 +146,72 @@ public class PaymentEndpoint {
         stripeSessionResponse.stripeSessionId = session.getId();
         return stripeSessionResponse;
     }
+
+    private StripeSessionCreateResponse createStripeSessionDepositFiatV2(String stripeSecretKey,
+                                                                       VxUser user,
+                                                                       VxStripeConfig userStripeConfig,
+                                                                       PaymentDepositFiatParams depositFiatParams) throws
+            StripeException {
+
+        Stripe.apiKey = stripeSecretKey;
+
+        Double _3procent = (3.0 * Double.valueOf(depositFiatParams.amount)) / 100.0;
+        double messageValue = (Double.valueOf(depositFiatParams.amount) - _3procent) / 100.0; // 2 decimals
+
+        String message = String.format("Deposit fiat: %.2f %s for %s, ", messageValue, depositFiatParams.currency, user.email);
+
+        PriceCreateParams priceParams = PriceCreateParams.builder()
+                .setCurrency(depositFiatParams.currency)
+                .setProductData(
+                        PriceCreateParams.ProductData.builder()
+                                .setName(message)
+                                .build()
+                )
+                .setUnitAmount(depositFiatParams.amount)
+                .build();
+
+        Price price = Price.create(priceParams);
+
+        // Line item details
+        SessionCreateParams.LineItem.Builder lineItemBuilder = SessionCreateParams.LineItem.builder()
+                .setPrice(price.getId()) // Use the ID of the dynamically created price
+                .setQuantity(1L);
+
+        String successUrl = String.format("http://localhost:3000/vxpayment/sucess?paymentId=%s", "depositFiat");
+        String cancelUrl = String.format("http://localhost:3000/vxpayment/cancel?paymentId=%s", "depositFiat");
+
+        // Session parameters
+        SessionCreateParams.Builder paramsBuilder = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .addLineItem(lineItemBuilder.build())
+                .setSuccessUrl(successUrl)
+                .setCancelUrl(cancelUrl);
+
+        SessionCreateParams.PaymentIntentData.Builder paymentIntentDataBuilder = SessionCreateParams.PaymentIntentData.builder();
+
+        // You can add payment intent data as needed
+        // paymentIntentDataBuilder.setApplicationFeeAmount(123L)
+        //         .setTransferData(
+        //                 SessionCreateParams.PaymentIntentData.TransferData.builder()
+        //                         .setDestination("{{CONNECTED_ACCOUNT_ID}}")
+        //                         .build()
+        //         );
+
+        paramsBuilder.setPaymentIntentData(paymentIntentDataBuilder.build());
+
+        SessionCreateParams params = paramsBuilder.build();
+
+        Session session = Session.create(params);
+        System.out.println("Checkout Session URL: " + session.getUrl());
+        System.out.println("StripeSessionId = " + session.getId());
+        System.out.println("paymentId");
+
+        StripeSessionCreateResponse stripeSessionResponse = new StripeSessionCreateResponse();
+        stripeSessionResponse.url = session.getUrl();
+        stripeSessionResponse.stripeSessionId = session.getId();
+        return stripeSessionResponse;
+    }
+
 
 
 }
