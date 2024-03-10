7bb4f70813b4266d364c82f86046bb80a5648d74
Bogdan <bogdan.oloeriu@gmail.com>
2024-03-10 21:17:03 +0100

    added deposit funds form

--- vxbank-web-app-v2/src/app/components/profile/DepositFiatModal.js
+++ vxbank-web-app-v2/src/app/components/profile/DepositFiatModal.js
@@ -45,10 +45,40 @@ const DepositFiatModal = ({ currency }) => {
       >
         <div className="relative p-4 w-full max-w-md max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button
+              type="button"
+              className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="deposit-modal"
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
             <div className="p-4 md:p-5 text-center">
               <h3 className="pt-8 mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Hello deposit modal
+                Deposit {currency}
               </h3>
+              <p className="text-gray-600 mb-8 dark:text-gray-300">
+                Please enter the amount you want to deposit:
+              </p>
+              <input
+                type="number"
+                placeholder="Enter amount"
+                className="border border-gray-200 rounded-lg px-4 py-2 focus:outline-none focus:border-blue-500"
+              />
               <div className="mt-6 flex justify-center">
                 <button
                   data-modal-hide="deposit-modal"
@@ -60,8 +90,9 @@ const DepositFiatModal = ({ currency }) => {
                 <button
                   type="button"
                   className="py-2.5 px-5 ms-3 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500"
+                  
                 >
-                  Ok
+                  Deposit
                 </button>
               </div>
             </div>
