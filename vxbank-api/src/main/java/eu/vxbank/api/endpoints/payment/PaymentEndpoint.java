10d15edaac545bec8a18fb2fc86c0c6f19a38ca4
Bogdan <bogdan.oloeriu@gmail.com>
2024-03-10 18:31:49 +0100

    no currency stored in datastore

--- vxbank-api/src/main/java/eu/vxbank/api/endpoints/payment/PaymentEndpoint.java
+++ vxbank-api/src/main/java/eu/vxbank/api/endpoints/payment/PaymentEndpoint.java
@@ -1,21 +1,30 @@
 package eu.vxbank.api.endpoints.payment;
 
 
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
 import eu.vxbank.api.endpoints.payment.dto.PaymentCreateParams;
 import eu.vxbank.api.endpoints.payment.dto.PaymentDepositFiatParams;
 import eu.vxbank.api.endpoints.payment.dto.StripeSessionCreateResponse;
+import eu.vxbank.api.endpoints.user.dto.LoginResponse;
 import eu.vxbank.api.utils.components.SystemService;
 import eu.vxbank.api.utils.components.VxStripeKeys;
 import eu.vxbank.api.utils.stripe.VxStripeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import vxbank.datastore.VxBankDatastore;
 import vxbank.datastore.data.models.VxPayment;
+import vxbank.datastore.data.models.VxStripeConfig;
+import vxbank.datastore.data.models.VxUser;
 import vxbank.datastore.data.service.VxDsService;
+
+import java.util.*;
 
 @RestController
 public class PaymentEndpoint {
@@ -45,12 +54,90 @@ public class PaymentEndpoint {
     @PostMapping("/payment/depositFiat")
     @ResponseBody
     public StripeSessionCreateResponse depositFiat(
+            Authentication authentication,
             @RequestBody PaymentDepositFiatParams params
     ) throws StripeException {
+
+
+        Jwt jwtToken = (Jwt) authentication.getPrincipal();
+        String email = jwtToken.getClaim("email");
+
+
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.id = Long.valueOf(authentication.getName());
+        loginResponse.email = email;
+
+        VxBankDatastore ds = systemService.getVxBankDatastore();
+        List<VxStripeConfig> configList = VxDsService.getByUserId(loginResponse.id,
+                new HashMap<>(),
+                ds,
+                VxStripeConfig.class);
+        Optional<VxStripeConfig> optionalConfig = configList.stream().filter(
+                        item -> item.currency.equals(params.currency))
+                .findFirst();
+        if (optionalConfig.isEmpty()) {
+            throw new IllegalStateException("User needs to configure first bank for currency=" + params.currency);
+        }
+
+        VxStripeConfig stripeConfig = optionalConfig.get();
+        VxUser user = VxDsService.getById(VxUser.class, ds, loginResponse.id);
+
+        StripeSessionCreateResponse stripeSessionCreateResponse = createStripeSessionDepositFiat(
+                stripeKeys.stripeSecretKey,
+                user,
+                stripeConfig,
+                params);
 
         throw new IllegalStateException("Please implement this");
     }
 
+    private StripeSessionCreateResponse createStripeSessionDepositFiat(String stripeSecretKey,
+                                                                       VxUser user,
+                                                                       VxStripeConfig userStripeConfig,
+                                                                       PaymentDepositFiatParams depositFiatParams) throws
+            StripeException {
+
+        Stripe.apiKey = stripeSecretKey;
+
+        Long _3procent = (3L * depositFiatParams.amount) / 100;
+        Long messageValue = depositFiatParams.amount - _3procent;
+
+        String message = String.format("Deposit fiat: %d %s for %s, ", messageValue, depositFiatParams.currency, user.email);
+
+        // Line item details
+        Map<String, Object> priceData = new HashMap<>();
+        priceData.put("currency", depositFiatParams.currency);
+        priceData.put("product_data", Map.of("name", message));
+        priceData.put("unit_amount", depositFiatParams.amount);
+
+        Map<String, Object> lineItem = new HashMap<>();
+        lineItem.put("price_data", priceData);
+        lineItem.put("quantity", 1);
+
+        // Line items list
+        List<Object> lineItems = new ArrayList<>();
+        lineItems.add(lineItem);
+
+        String successUrl = String.format("http://localhost:3000/vxpayment/sucess?paymentId=%s", "depositFiat");
+        String cancelUrl = String.format("http://localhost:3000/vxpayment/cancel?paymentId=%s", "depositFiat");
+
+        // Session parameters
+        Map<String, Object> params = new HashMap<>();
+        params.put("line_items", lineItems);
+        params.put("success_url", successUrl);
+        params.put("cancel_url", cancelUrl);
+        params.put("mode", "payment");
+
+        Session session = Session.create(params);
+        System.out.println("Checkout Session URL: " + session.getUrl());
+        System.out.println("StripeSessionId = " + session.getId());
+        System.out.println("paymentId");
+
+        StripeSessionCreateResponse stripeSessionResponse = new StripeSessionCreateResponse();
+        stripeSessionResponse.url = session.getUrl();
+        stripeSessionResponse.stripeSessionId = session.getId();
+        return stripeSessionResponse;
+    }
 
 
 }
