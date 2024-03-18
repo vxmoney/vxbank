5df98e5d9f14587576952c8bb26048086eb60fdc
Bogdan <bogdan.oloeriu@gmail.com>
2024-03-18 22:08:07 +0100

    added pay join event

--- vxbank-web-app-v2/src/app/components/events1v1/Join1v1EventModal.js
+++ vxbank-web-app-v2/src/app/components/events1v1/Join1v1EventModal.js
@@ -225,6 +225,70 @@ const Join1v1EventModal = ({ fetchParticipants }) => {
         </div>
       </div>
 
+      {/* Pay Join Modal */}
+
+      <button
+        type="button"
+        data-modal-target="pay-join-modal"
+        data-modal-toggle="pay-join-modal"
+        className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+      >
+        Pay join event
+      </button>
+
+      <div
+        id="pay-join-modal"
+        tabIndex="-1"
+        className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
+        <div className="relative p-4 w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button
+              type="button"
+              className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="pay-join-modal"
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="p-4 md:p-5 text-center">
+              <h3 className="pt-8 mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                Are you sure you want to pay and join this event?
+              </h3>
+              <button
+                type="button"
+                data-modal-hide="pay-join-modal"
+                className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                onClick={handleJoinSubmit}
+              >
+                Yes, I&apos;m sure
+              </button>
+              <button
+                data-modal-hide="pay-join-modal"
+                type="button"
+                className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              >
+                No, cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Close Event Modal */}
       <button
         type="button"
