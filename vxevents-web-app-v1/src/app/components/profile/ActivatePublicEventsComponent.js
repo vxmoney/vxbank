b4aa9d4fa329402b6c2f24a4f5cd6f287023a3a6
Bogdan <bogdan.oloeriu@gmail.com>
2024-03-31 07:38:13 +0200

    show hide using ctrl + m

--- vxevents-web-app-v1/src/app/components/profile/ActivatePublicEventsComponent.js
+++ vxevents-web-app-v1/src/app/components/profile/ActivatePublicEventsComponent.js
@@ -1,19 +1,49 @@
-const ActivatePublicEventsComponent = () => {
-  return (
-    <div className="pt-8 pl-8 pr-8">
-      <div className="block  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Activate manage public events features
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400k">
-          You need to update stripe configuration
-        </p>
+import { useState, useEffect } from "react";
 
-        <div className="flex-grow overflow-auto">
-          You will be redirected to vxbank stripe configuration page in order to
-          update your configuration
+const ActivatePublicEventsComponent = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.ctrlKey && event.key === "m") {
+        setIsVisible(!isVisible); // Toggle visibility
+        console.log(
+          "Ctrl+M pressed, currentVisibility: ",
+          isVisible ? "visible" : "hidden "
+        );
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [isVisible]); // Add isVisible to dependency array
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div>
+      {isVisible && (
+        <div className="pt-8 pl-8 pr-8">
+          <div className="block  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Activate manage public events features
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400k">
+              Nothing fancy here, just a simple card
+            </p>
+
+            <div className="flex-grow overflow-auto">
+              Use this to activate public events features. You can turn this off
+              at any time
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
