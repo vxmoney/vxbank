93eb31c49d439bced28b8650bdac150a7f671abc
Bogdan <bogdan.oloeriu@gmail.com>
2024-04-03 19:56:06 +0200

    changed the stile of the button

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/OnboardClientsComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/OnboardClientsComponent.js
@@ -22,7 +22,7 @@ export default function OnboardClientsComponent() {
             Use this to onboard new clients
           </p>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             onClick={openModal}
           >
             Open Modal
@@ -44,7 +44,7 @@ export default function OnboardClientsComponent() {
               </div>
               <div className="mt-6 text-center">
                 <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                   onClick={closeModal}
                 >
                   Close
