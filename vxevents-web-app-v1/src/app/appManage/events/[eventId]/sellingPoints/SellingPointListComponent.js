70100a32dbcee7fcc2c25c5f913acbbf7eb1d45c
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-10 14:17:20 +0200

    formated the table

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sellingPoints/SellingPointListComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sellingPoints/SellingPointListComponent.js
@@ -1,22 +1,24 @@
-"use client"
+"use client";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { UserAuth } from "@/app/context/AuthContext";
 import { useVxContext } from "@/app/context/VxContext";
 
 export default function SellingPointListComponent() {
+  const { vxUserInfo } = UserAuth();
+  const { eventId } = useParams();
+  const { sellingPoints, fetchSellingPoints } = useVxContext();
 
-    const { vxUserInfo } = UserAuth();
-    const { eventId } = useParams();
-    const { sellingPoints, fetchSellingPoints } = useVxContext();
+  useEffect(() => {
+    // Check if vxToken is available before calling fetchEvents
+    if (vxUserInfo && vxUserInfo?.vxToken) {
+      fetchSellingPoints(vxUserInfo?.vxToken, eventId);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    useEffect(() => {
-        // Check if vxToken is available before calling fetchEvents
-        if (vxUserInfo && vxUserInfo?.vxToken) {
-          fetchSellingPoints(vxUserInfo?.vxToken, eventId);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
+  console.log("sellingPoints DEBUG 1", sellingPoints);
 
   return (
     <div className="p-2 relative overflow-x-auto">
@@ -32,13 +34,23 @@ export default function SellingPointListComponent() {
             <th scope="col" className="px-6 py-3">
               Products
             </th>
-            
           </tr>
         </thead>
         <tbody>
-          
+          {sellingPoints.map((sPoint) => (
+            <tr
+              key={sPoint.id}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            >
+              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {sPoint.id}
+              </td>
+              <td className="px-6 py-4">{sPoint.title}</td>
+              <td className="px-6 py-4"> list of products goes here</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-);
+  );
 }
