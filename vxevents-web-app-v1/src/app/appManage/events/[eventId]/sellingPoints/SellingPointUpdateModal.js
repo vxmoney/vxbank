0261e7a9804c9a349c55c7e49d8d242f95cfbd0b
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-10 15:27:50 +0200

    the lists look weird

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sellingPoints/SellingPointUpdateModal.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sellingPoints/SellingPointUpdateModal.js
@@ -13,15 +13,19 @@ export default function SellingPointUpdateModal({
   const { vxUserInfo } = UserAuth();
   const [modalOpen, setModalOpen] = useState(false);
   const { fetchSellingPoints } = useVxContext();
-  
+
   const [updateParams, setUpdateParams] = useState({
     vxUserId: vxUserInfo?.id,
     title: initialTitle || "Selling point 001",
   });
-  
-  const [selectedProducts, setSelectedProducts] = useState(initialSelectedProducts || []);
+
+  const [selectedProducts, setSelectedProducts] = useState(
+    initialSelectedProducts || []
+  );
   const [missingProducts, setMissingProducts] = useState(
-    allProducts.filter(product => !initialSelectedProducts.some(p => p.id === product.id))
+    allProducts.filter(
+      (product) => !initialSelectedProducts.some((p) => p.id === product.id)
+    )
   );
 
   const modalRef = useRef(null);
@@ -41,7 +45,7 @@ export default function SellingPointUpdateModal({
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
-    setUpdateParams(prev => ({ ...prev, [id]: value }));
+    setUpdateParams((prev) => ({ ...prev, [id]: value }));
   };
 
   const openModal = () => setModalOpen(true);
@@ -51,27 +55,30 @@ export default function SellingPointUpdateModal({
 
   const addItemToSelectedProducts = (product) => {
     setSelectedProducts([...selectedProducts, product]);
-    setMissingProducts(missingProducts.filter(p => p.id !== product.id));
+    setMissingProducts(missingProducts.filter((p) => p.id !== product.id));
   };
 
   const removeFromSelectedProducts = (product) => {
     setMissingProducts([...missingProducts, product]);
-    setSelectedProducts(selectedProducts.filter(p => p.id !== product.id));
+    setSelectedProducts(selectedProducts.filter((p) => p.id !== product.id));
   };
 
-  const getProductIdList = () => selectedProducts.map(product => product.id);
+  const getProductIdList = () => selectedProducts.map((product) => product.id);
 
   const callUpdateSellingPoint = () => {
-    publicEventSellingPointAPI.update(vxUserInfo.vxToken, sellingPointId, {
-      title: updateParams.title,
-      productIdList: getProductIdList(),
-    }).then(response => {
-      console.log("Selling point updated:", response.data);
-      closeModal();
-      fetchSellingPoints(vxUserInfo.vxToken);
-    }).catch(error => {
-      console.error("Error updating selling point:", error);
-    });
+    publicEventSellingPointAPI
+      .update(vxUserInfo.vxToken, sellingPointId, {
+        title: updateParams.title,
+        productIdList: getProductIdList(),
+      })
+      .then((response) => {
+        console.log("Selling point updated:", response.data);
+        closeModal();
+        fetchSellingPoints(vxUserInfo.vxToken);
+      })
+      .catch((error) => {
+        console.error("Error updating selling point:", error);
+      });
   };
 
   return (
@@ -79,24 +86,98 @@ export default function SellingPointUpdateModal({
       {modalOpen && (
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex items-center justify-center min-h-screen">
-            <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-              <div className={`absolute inset-0 bg-gray-500 opacity-50`}></div>
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div
+                className={`absolute inset-0 ${
+                  modalOpen ? "bg-gray-500 opacity-50" : ""
+                } ${modalOpen ? "dark:bg-gray-800 dark:opacity-50" : ""}`}
+              ></div>
             </div>
-            <div ref={modalRef} className="relative bg-white dark:bg-gray-900 rounded-lg p-8 max-w-md">
-              <h3 className="text-lg font-semibold mb-4">Update Selling Point</h3>
-              <div>
-                <input
-                  type="text"
-                  id="title"
-                  className="input-text"
-                  value={updateParams.title}
-                  onChange={handleInputChange}
-                  placeholder="Update title"
-                />
+            <div
+              ref={modalRef}
+              className="relative bg-white dark:bg-gray-900 rounded-lg p-8 max-w-md"
+            >
+              <div className="text-center">
+                <h3 className="text-lg font-semibold mb-4">
+                  Create Selling point
+                </h3>
               </div>
-              <div>
-                <button onClick={closeModal}>Cancel</button>
-                <button onClick={callUpdateSellingPoint}>Update</button>
+              <div className="mt-6 text-center">
+                <form
+                  className="p-4 md:p-5 border-b rounded-t dark:border-gray-600"
+                  // onSubmit={handleSubmit}
+                >
+                  <div className="flex items-center mb-6 flex-no-wrap">
+                    {/* Title Section */}
+                    <div className="flex items-center mr-6">
+                      <label
+                        htmlFor="title"
+                        className="mb-0 mr-2 text-sm font-medium text-gray-900 dark:text-white shrink-0"
+                      >
+                        Selling point title
+                      </label>
+                      <input
+                        type="text"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Placeholder"
+                        required
+                        id="title"
+                        value={updateParams.title}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+                  </div>
+                </form>
+
+                {/* selected products */}
+                <div>
+                  <div>Selected products</div>
+                  <div className="flex flex-wrap gap-2 pt-2 pb-2 border-b dark:border-gray-600">
+                    {selectedProducts.map((product) => (
+                      <span
+                        key={product.id}
+                        className="cursor-pointer bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300"
+                        onClick={() => removeFromSelectedProducts(product)}
+                      >
+                        {`${product.title} €${formatPrice(product.price)}`}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+
+                {/* missing products */}
+                <div>
+                  <div>Missing products</div>
+                  <div className="flex flex-wrap gap-2 pt-2 pb-2 border-b dark:border-gray-600">
+                    {missingProducts.map((product) => (
+                      <span
+                        key={product.id}
+                        className="cursor-pointer bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300"
+                        onClick={() => addItemToSelectedProducts(product)}
+                      >
+                        {`${product.title} €${formatPrice(product.price)}`}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="flex pt-4 justify-center">
+                  <button
+                    className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    onClick={closeModal}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-blue-500 hover:text-white focus:z-10 focus:ring-4 focus:ring-blue-500 dark:focus:ring-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-blue-700"
+                    onClick={callUpdateSellingPoint}
+                  >
+                    Update
+                  </button>
+                </div>
               </div>
             </div>
           </div>
