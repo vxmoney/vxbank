89e93ff7adfb3e18d7a2c5bf789982d05de296d2
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 18:22:36 +0200

    simpler layout

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductListComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductListComponent.js
@@ -33,54 +33,23 @@ export default function SellProductListComponent() {
   let productList = null;
   if (sellingPoint && sellingPoint.productList) {
     productList = sellingPoint.productList.map((product) => (
-      <div key={product.id}>
-        <p>ID: {product.id}</p>
-        <h3>Title: {product.title}</h3>
+      <div key={product.id} class="p-4 m-4 flex flex-col items-center justify-center">
+        <div class=" text-3xl font-extrabold whitespace-nowrap">{product.title}</div>
       </div>
     ));
   }
 
   return (
-    <div class="block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="block max-w-xl rounded-lg ">
       <div
-        class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800"
+        class="bg-white dark:bg-gray-800"
         id="stats"
         role="tabpanel"
         aria-labelledby="stats-tab"
       >
-        {productList}
 
         <dl class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
-          <div class="flex flex-col items-center justify-center">
-            <dt class="mb-2 text-3xl font-extrabold">Cofey 1</dt>
-            <dd class="text-gray-500 dark:text-gray-400">Developers</dd>
-          </div>
-          <div class="flex flex-col items-center justify-center">
-            <dt class="mb-2 text-3xl font-extrabold">100M+</dt>
-            <dd class="text-gray-500 dark:text-gray-400">
-              Public repositories
-            </dd>
-          </div>
-          <div class="flex flex-col items-center justify-center">
-            <dt class="mb-2 text-3xl font-extrabold">1000s</dt>
-            <dd class="text-gray-500 dark:text-gray-400">
-              Open source projects
-            </dd>
-          </div>
-          <div class="flex flex-col items-center justify-center">
-            <dt class="mb-2 text-3xl font-extrabold">1B+</dt>
-            <dd class="text-gray-500 dark:text-gray-400">Contributors</dd>
-          </div>
-          <div class="flex flex-col items-center justify-center">
-            <dt class="mb-2 text-3xl font-extrabold">90+</dt>
-            <dd class="text-gray-500 dark:text-gray-400">
-              Top Forbes companies
-            </dd>
-          </div>
-          <div class="flex flex-col items-center justify-center">
-            <dt class="mb-2 text-3xl font-extrabold">4M+</dt>
-            <dd class="text-gray-500 dark:text-gray-400">Organizations</dd>
-          </div>
+          {productList}
         </dl>
       </div>
     </div>
