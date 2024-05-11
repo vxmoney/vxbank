c9456fe69a8313039be0ab4a82f1f9d302c32c54
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 22:44:00 +0200

    prep to add click event

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellToolBarComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellToolBarComponent.js
@@ -43,22 +43,7 @@ export default function SellToolBarComponent() {
               Inline 2
             </label>
           </div>
-          <div className="flex items-center me-4">
-            <input
-              // checked
-              id="inline-checked-radio"
-              type="radio"
-              value=""
-              name="inline-radio-group"
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />
-            <label
-              htmlFor="inline-checked-radio"
-              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Inline checked
-            </label>
-          </div>
+          
         </div>
       </div>
     </div>
