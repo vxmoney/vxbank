b2af5017ceed724ad9b44074f9c3fa77c4e3278f
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 22:49:00 +0200

    changed the value

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellToolBarComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellToolBarComponent.js
@@ -4,23 +4,31 @@ import { useSellContext } from "@/app/context/SellContext";
 export default function SellToolBarComponent() {
   const { addItems, setAddItems } = useSellContext();
 
-  // Function to compute the total price of all items in the sellItemList array
+  // Function to handle radio change
+  const handleRadioChange = (value) => {
+    setAddItems(value === "true"); // Parse the string value to boolean and update addItems
+  };
+
+  console.log(addItems);
 
   return (
     <div
-      href="#"
       className="block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
       <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+
+        
         <div className="flex">
           <div className="flex items-center me-4">
             <input
               id="inline-radio"
               type="radio"
-              value=""
+              value="true"
               name="inline-radio-group"
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />{" "}
+              checked={addItems === true}
+              onChange={() => handleRadioChange("true")}
+            />
             <label
               htmlFor="inline-radio"
               className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -32,9 +40,11 @@ export default function SellToolBarComponent() {
             <input
               id="inline-2-radio"
               type="radio"
-              value=""
+              value="false"
               name="inline-radio-group"
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              checked={addItems === false}
+              onChange={() => handleRadioChange("false")}
             />
             <label
               htmlFor="inline-2-radio"
@@ -43,7 +53,6 @@ export default function SellToolBarComponent() {
               Inline 2
             </label>
           </div>
-          
         </div>
       </div>
     </div>
