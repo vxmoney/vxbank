aa9ad4b587b93af7f5c7924e93bd93e1318bbe5e
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-12 00:14:35 +0200

    added submit

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
@@ -5,10 +5,27 @@ import { BiReset } from "react-icons/bi";
 import { FaCheckCircle, FaCheckDouble } from "react-icons/fa";
 
 export default function SellTotalComponent() {
-  const { sellItemList, showToolBar, setShowToolBar } = useSellContext();
+  const {
+    sellItemList,
+    showToolBar,
+    setShowToolBar,
+    resetSelectedItems,
+    setAddItems,
+  } = useSellContext();
 
   const switchToolBar = () => {
     setShowToolBar(!showToolBar);
+  };
+
+  const handleReset = () => {
+    resetSelectedItems();
+    setAddItems(true);
+  };
+
+  const handleSubmit = () => {
+    console.log("Submit");
+    resetSelectedItems();
+    setAddItems(true);
   };
 
   // Function to compute the total price of all items in the sellItemList array
@@ -24,15 +41,12 @@ export default function SellTotalComponent() {
 
   return (
     <div className="flex gap-4 items-center block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <BiReset onClick={handleReset} />{" "}
       <MdOutlineSettings onClick={switchToolBar} />
-
-      <BiReset />
-
-      <h5 className="ml-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white mr-4">
+      <h5 className="ml-6 text-2xl font-bold tracking-tight text-gray-900 dark:text-white mr-4">
         {formatPrice(totalPrice)} Euro
       </h5>
-
-      <FaCheckDouble />
+      <FaCheckDouble onClick={handleSubmit} />
     </div>
   );
 }
