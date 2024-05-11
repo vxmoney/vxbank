dc38cb7a9f01eeae75dad2870ac7c4b9a7da5c12
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 19:58:40 +0200

    computed the value

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
@@ -1,15 +1,28 @@
 "use client";
+import { useSellContext } from "@/app/context/SellContext";
 
 export default function SellTotalComponent() {
+  const { sellItemList } = useSellContext();
+
+  // Function to compute the total price of all items in the sellItemList array
+function computeTotalPrice(items) {
+  return items.reduce((total, item) => {
+      return total + item.price;
+  }, 0);
+}
+
+const formatPrice = (price) => `${(price / 100).toFixed(2)}`;
+
+const totalPrice = computeTotalPrice(sellItemList);
+
   return (
     <div
       href="#"
       class="block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        125.10 Euro
+        {formatPrice(totalPrice)} Euro
       </h5>
-      
     </div>
   );
 }
