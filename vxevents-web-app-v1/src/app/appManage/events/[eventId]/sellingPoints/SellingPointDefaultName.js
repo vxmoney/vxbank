c406f5aed24442afad826a66a998ca9a2f7c698d
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 15:13:14 +0200

    removed debug lines

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sellingPoints/SellingPointDefaultName.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sellingPoints/SellingPointDefaultName.js
@@ -27,15 +27,13 @@ export default function SellingPointDefaultName() {
     publicEventSellingPointAPI
       .get(vxUserInfo.vxToken, getDefaultSellingPointId())
       .then((result) => {
-        console.log("DEBUG result data", result.data);
         setSellingPoint(result.data);
       });
   }, [defaultSellingPointId]);
 
-  console.log("DEBUG sellingPoint value", sellingPoint);
 
   // if defaultSellingPointId is not set return
-  if (defaultSellingPointId === "null") {
+  if (sellingPoint === null) {
     return (
       <div>
         <h1>No default selling point</h1>
@@ -45,7 +43,7 @@ export default function SellingPointDefaultName() {
 
   return (
     <div>
-      <h1>Default selling point id: {defaultSellingPointId}</h1>
+      <h1>Default: {sellingPoint.title}</h1>
     </div>
   );
 }
