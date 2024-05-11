be4e0c51c541537de1af38924065b89a4f5e309e
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-12 00:37:19 +0200

    I added check also on button

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
@@ -2,7 +2,7 @@
 import { useSellContext } from "@/app/context/SellContext";
 import { MdOutlineSettings } from "react-icons/md";
 import { BiReset } from "react-icons/bi";
-import { FaCheckCircle, FaCheckDouble } from "react-icons/fa";
+import { FaCheckDouble } from "react-icons/fa";
 
 export default function SellTotalComponent() {
   const {
@@ -39,6 +39,24 @@ export default function SellTotalComponent() {
 
   const totalPrice = computeTotalPrice(sellItemList);
 
+  let okButton = (
+    <FaCheckDouble
+      className="w-10 h-10 stroke-current text-gray-500" // Disabled state
+    />
+  );
+  
+  /**
+   * If sellItemList is not empty, then add the okButton to the component with click functionality
+   */
+  if (sellItemList.length > 0) {
+    okButton = (
+      <FaCheckDouble
+        className="w-10 h-10 stroke-current text-green-500 cursor-pointer" // Enabled state with a suitable color for 'ok'
+        onClick={handleSubmit}
+      />
+    );
+  }
+
   return (
     <div className="flex gap-4 items-center block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <BiReset onClick={handleReset} />{" "}
@@ -46,7 +64,7 @@ export default function SellTotalComponent() {
       <h5 className="ml-6 text-2xl font-bold tracking-tight text-gray-900 dark:text-white mr-4">
         {formatPrice(totalPrice)} Euro
       </h5>
-      <FaCheckDouble onClick={handleSubmit} />
+      {okButton}
     </div>
   );
 }
