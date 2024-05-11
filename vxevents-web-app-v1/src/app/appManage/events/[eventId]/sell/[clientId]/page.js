25d3bae6fdac14b4845d791d7d1e9c63c8352405
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 15:38:05 +0200

    loaded selling point from context

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/page.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/page.js
@@ -1,14 +1,41 @@
 "use client";
 import { useParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useVxContext } from "@/app/context/VxContext";
+import { publicEventSellingPointAPI } from "@/api/publicEventSellingPoint";
+import { UserAuth } from "@/app/context/AuthContext";
 
 export default function SellManagePage() {
   let { eventId, clientId } = useParams();
+  const { vxUserInfo } = UserAuth();
+  const { defaultSellingPointId, setDefaultSellingPointId } = useVxContext();
+  const [sellingPoint, setSellingPoint] = useState(null);
+
+  // create a method that return default selling point id but as a number that passes this check typeof pointId !== 'number'
+  const getDefaultSellingPointId = () => {
+    if (typeof defaultSellingPointId !== "number") {
+      return parseInt(defaultSellingPointId);
+    }
+    return defaultSellingPointId;
+  };
+
+  useEffect(() => {
+    if (defaultSellingPointId === "null") {
+      return;
+    }
+    publicEventSellingPointAPI
+      .get(vxUserInfo.vxToken, getDefaultSellingPointId())
+      .then((result) => {
+        setSellingPoint(result.data);
+      });
+  }, [defaultSellingPointId]);
 
   return (
     <>
       <div>Hello sell page</div>
       <div>Event ID: {eventId}</div>
       <div>Client ID: {clientId}</div>
+      <div>Default selling point id: {defaultSellingPointId}</div>
     </>
   );
 }
