919a0652f5a010dbc0a50f34bd394c95bfa27543
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 22:07:33 +0200

    wired back append item

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductButtonComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductButtonComponent.js
@@ -3,10 +3,10 @@
 import { useSellContext } from "@/app/context/SellContext";
 
 export default function SellProductButtonComponent({ product }) {
-  const { sellItemList, appendSellItem } = useSellContext();
+  const { sellItemList, processItem } = useSellContext();
 
-  const handleAppendItem = () => {
-    appendSellItem(product);
+  const handleProcessItem = () => {
+    processItem(product);
   };
 
   let productCount = null;
@@ -33,7 +33,7 @@ export default function SellProductButtonComponent({ product }) {
       <button
         type="button"
         class="relative text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 whitespace-nowrap"
-        onClick={handleAppendItem}
+        onClick={handleProcessItem}
       >
         {product.title}
 
