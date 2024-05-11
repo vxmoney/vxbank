a36c66bb4780d429292f68be574f496d53e2fc12
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-11 19:15:06 +0200

    added button list

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductButtonComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductButtonComponent.js
@@ -1,7 +1,14 @@
 "use client";
 
-export default function SellProductButtonComponent(){
-    return {
-        
-    }
+export default function SellProductButtonComponent({ product }) {
+  return (
+    <div class="flex flex-col items-center justify-center">
+      <button
+        type="button"
+        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 whitespace-nowrap"
+      >
+        {product.title}
+      </button>
+    </div>
+  );
 }
--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductListComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellProductListComponent.js
@@ -5,6 +5,7 @@ import { useVxContext } from "@/app/context/VxContext";
 import { publicEventSellingPointAPI } from "@/api/publicEventSellingPoint";
 import { UserAuth } from "@/app/context/AuthContext";
 import { useSellContext } from "@/app/context/SellContext";
+import SellProductButtonComponent from "./SellProductButtonComponent";
 
 export default function SellProductListComponent() {
   const { vxUserInfo } = UserAuth();
@@ -30,31 +31,22 @@ export default function SellProductListComponent() {
       });
   }, [defaultSellingPointId]);
 
-  let productList = null;
+  let buttonList = null;
   if (sellingPoint && sellingPoint.productList) {
-    productList = sellingPoint.productList.map((product) => (
-      <div
+    buttonList = sellingPoint.productList.map((product) => (
+      <SellProductButtonComponent
+        product={product}
         key={product.id}
         class="flex flex-col items-center justify-center"
-      >
-        <button
-          type="button"
-          class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 whitespace-nowrap"
-        >
-          {product.title}
-        </button>
-      </div>
+      />
     ));
   }
 
   return (
     <div class="block rounded-lg ">
-      <div
-        class="bg-white dark:bg-gray-800 rounded-lg"
-        
-      >
+      <div class="p-2 bg-white dark:bg-gray-800 rounded-lg">
         <dl class="grid max-w-screen-xl grid-cols-3 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
-          {productList}
+          {buttonList}
         </dl>
       </div>
     </div>
