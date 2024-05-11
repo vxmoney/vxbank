ab7a9b46bd7e2548d5cae0c675c796fa70f97e8c
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-12 00:06:03 +0200

    added some icons

--- vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
+++ vxevents-web-app-v1/src/app/appManage/events/[eventId]/sell/[clientId]/SellTotalComponent.js
@@ -1,6 +1,8 @@
 "use client";
 import { useSellContext } from "@/app/context/SellContext";
 import { MdOutlineSettings } from "react-icons/md";
+import { BiReset } from "react-icons/bi";
+import { FaCheckCircle, FaCheckDouble } from "react-icons/fa";
 
 export default function SellTotalComponent() {
   const { sellItemList, showToolBar, setShowToolBar } = useSellContext();
@@ -21,11 +23,16 @@ export default function SellTotalComponent() {
   const totalPrice = computeTotalPrice(sellItemList);
 
   return (
-    <div className="flex items-center block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mr-4">
+    <div className="flex gap-4 items-center block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <MdOutlineSettings onClick={switchToolBar} />
+
+      <BiReset />
+
+      <h5 className="ml-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white mr-4">
         {formatPrice(totalPrice)} Euro
       </h5>
-      <MdOutlineSettings onClick={switchToolBar}/>
+
+      <FaCheckDouble />
     </div>
   );
 }
