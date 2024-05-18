6d1b08609377dcf6e732fa1cc897b46c23863a23
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-18 11:41:48 +0200

    wired vx_evetns

--- vxevents-contract/src/vx_events.rs
+++ vxevents-contract/src/vx_events.rs
@@ -8,7 +8,7 @@ mod vx_events_endpoints;
 
 /// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
-pub trait VxEvents: admin_tools::AdminTools {
+pub trait VxEvents: admin_tools::AdminTools + vx_events_endpoints::EventsEndpoints {
     #[init]
     fn init(&self) {}
 
