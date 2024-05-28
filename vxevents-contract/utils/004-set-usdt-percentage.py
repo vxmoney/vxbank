f9cc9a82246120d0ea9f914c54ea0ad684570ee1
Bogdan <bogdan.oloeriu@gmail.com>
2024-05-28 19:37:05 +0200

    signed the transaction

--- vxevents-contract/utils/004-set-usdt-percentage.py
+++ vxevents-contract/utils/004-set-usdt-percentage.py
@@ -25,10 +25,44 @@ def load_value_from_file(filename):
         return None
 
 def main():
-    contract_address = load_value_from_file("./utils/contract_address.txt")
-    token_identifier = load_value_from_file("./utils/token_identifier.txt")
-    print(f"loaded contract_address = {contract_address}")
-    print(f"loaded token_identifier = {token_identifier}")
+
+    provider = ProxyNetworkProvider(SIMULATOR_URL)
+    ownerPem = UserPEM.from_file(Path("./utils/simwalets/aliceWallet.pem"))
+    ownerAddress = ownerPem.public_key.to_address("erd")
+
+    config = TransactionsFactoryConfig(provider.get_network_config().chain_id)
+    sc_factory = SmartContractTransactionsFactory(config, TokenComputer())
+
+    provider.do_post(f"{GENERATE_BLOCKS_URL}/1", {})
+
+    contract_address_B32 = load_value_from_file("./utils/contract_address.txt")
+    token_identifier_B32 = load_value_from_file("./utils/token_identifier.txt")
+    token_percentage = 10000
+    print(f"loaded contract_address_B32 = {contract_address_B32}")
+    print(f"loaded token_identifier = {token_identifier_B32}")
+
+
+    contract_address = Address.new_from_bech32(contract_address_B32)
+    bad_call_transaction = sc_factory.create_transaction_for_execute(
+        sender=ownerAddress,
+        contract=contract_address,
+        function="add",
+        gas_limit=10000000,
+        arguments=["fake-token", token_percentage]
+    )
+    bad_call_transaction.nonce = provider.get_account(ownerAddress).nonce
+    user_signer = UserSigner(ownerPem.secret_key)
+    tx_computer = TransactionComputer()
+    bad_call_transaction.signature = user_signer.sign(tx_computer.compute_bytes_for_signing(bad_call_transaction))
+
+    # send transaction
+    tx_hash = provider.send_transaction(bad_call_transaction)
+    print(f"sc call tx hash: {tx_hash}")
+
+    # execute 1 block
+    time.sleep(0.5)
+    provider.do_post(f"{GENERATE_BLOCKS_URL}/1", {})
+
     print("Hello main")
 
 if __name__ == "__main__":
