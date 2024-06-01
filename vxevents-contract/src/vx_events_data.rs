ab8ec56a1bcf34dea813ac6f419bd2875b66a6e0
Bogdan <bogdan.oloeriu@gmail.com>
2024-06-01 22:10:19 +0200

    added token to payment

--- vxevents-contract/src/vx_events_data.rs
+++ vxevents-contract/src/vx_events_data.rs
@@ -3,7 +3,7 @@ multiversx_sc::derive_imports!();
 
 use multiversx_sc::{
     api::ManagedTypeApi,
-    types::{ManagedAddress, ManagedBuffer},
+    types::{ManagedAddress, ManagedBuffer,EgldOrEsdtTokenIdentifier},
 };
 
 #[derive(TypeAbi, TopEncode, TopDecode, NestedEncode, NestedDecode)]
@@ -11,8 +11,11 @@ pub struct Payment<M: ManagedTypeApi> {
     pub payment_id: ManagedBuffer<M>,
     pub event_id: ManagedBuffer<M>,
     pub payment_wallet: ManagedAddress<M>,
+    pub payment_token: EgldOrEsdtTokenIdentifier<M>,
     pub payment_amount: BigUint<M>,
     pub payment_total_value: BigUint<M>,
     pub payment_event_owner_value: BigUint<M>,
-    pub payment_event_fees: BigUint<M>
+    pub payment_event_fees: BigUint<M>,
 }
+
+
