0ab01cc19531275a1aadd2e4bde0230e3bb93b2c
Bogdan <bogdan.oloeriu@gmail.com>
2024-06-09 08:57:41 +0200

    not called the event

--- vxevents-contract/tests/vx_events_scenario_rs_test.rs
+++ vxevents-contract/tests/vx_events_scenario_rs_test.rs
@@ -27,3 +27,8 @@ fn t_003_set_egld_percentage_rs() {
 fn t_004_set_token_percentage_rs() {
     world().run("scenarios/t_004_set_token_percentage.scen.json");
 }
+
+#[test]
+fn t_006_create_event_rs() {
+    world().run("scenarios/t_006_create_event.scen.json");
+}
