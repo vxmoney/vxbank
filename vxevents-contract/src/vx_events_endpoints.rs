72673e7dad24b5874ac41f075d4454ec611aa562
Bogdan <bogdan.oloeriu@gmail.com>
2024-06-09 12:17:02 +0200

    reverted tests

--- vxevents-contract/src/vx_events_endpoints.rs
+++ vxevents-contract/src/vx_events_endpoints.rs
@@ -10,15 +10,15 @@ pub trait EventsEndpoints: vx_events_store::EventsStore {
         let _payment = self.call_value().egld_or_single_esdt();
     }
 
-    //#[endpoint(createEvent)]
+    #[endpoint(createEvent)]
     fn create_event_v1(&self, event_id: ManagedBuffer) {
         let _event_owner = self.blockchain().get_caller();
         sc_print!("debug line: Creating event with id: {}", event_id);
         self.store_event_owner_wallet(event_id).set(_event_owner);
     }
 
-    #[endpoint(createEvent)]
-    fn create_event(&self, _web2_id: ManagedBuffer) {
+    //#[endpoint(createEvent)]
+    fn create_event_count_id(&self, _web2_id: ManagedBuffer) {
         let _event_owner = self.blockchain().get_caller();
         let new_id = self.get_last_id() + 1;
 
