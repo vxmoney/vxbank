f7a391a00255d787b472dc69d3c92334280d8eed
Bogdan <bogdan.oloeriu@gmail.com>
2024-06-09 16:07:22 +0200

    removed old endpoints

--- vxevents-contract/src/vx_events_endpoints.rs
+++ vxevents-contract/src/vx_events_endpoints.rs
@@ -11,19 +11,9 @@ pub trait EventsEndpoints: vx_events_store::EventsStore {
     }
 
     #[endpoint(createEvent)]
-    fn create_event_v1(&self, event_id: ManagedBuffer) {
+    fn create_event(&self, event_id: ManagedBuffer) {
         let _event_owner = self.blockchain().get_caller();
         sc_print!("debug line: Creating event with id: {}", event_id);
         self.store_event_owner_wallet(event_id).set(_event_owner);
-    }
-
-    //#[endpoint(createEvent)]
-    fn create_event_count_id(&self, _web2_id: ManagedBuffer) {
-        let _event_owner = self.blockchain().get_caller();
-        let new_id = self.get_last_id() + 1;
-
-        sc_print!("debug line: new_id = {}", new_id);
-
-        self.store_event_owner_wallet_v2(new_id).set(_event_owner);
     }
 }
--- vxevents-contract/src/vx_events_store.rs
+++ vxevents-contract/src/vx_events_store.rs
@@ -13,14 +13,4 @@ pub trait EventsStore {
     #[storage_mapper("eventOwnerWallet")]
     fn store_event_owner_wallet(&self, event_id: ManagedBuffer) -> SingleValueMapper<ManagedAddress>;
     
-    #[storage_mapper("eventOwnerWalletV2")]
-    fn store_event_owner_wallet_v2(&self, event_id: u64) -> SingleValueMapper<ManagedAddress>;
-
-    #[storage_mapper("lastId")]
-    fn last_id(&self) -> SingleValueMapper<u64>;
-
-    #[view(getLastId)]
-    fn get_last_id(&self) -> u64 {
-        self.last_id().get() | 0
-    }
 }
